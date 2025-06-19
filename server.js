873613aa5b0e12933d9d3d92528f73bfa475591d
Suyash <suyashsingh2711@gmail.com>
2025-06-19 11:43:44 +0530

    intial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('require')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('MONGO DB CONNECTED'))
+    .catch((err) => console.log('MONGO DB ERROR', err));
+
+app.use('/api/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log('Server running on http://localhost: ${PORT}');
+});
